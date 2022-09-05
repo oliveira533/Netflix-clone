8d95a3b450cbc6ae01361a4f1df3820218b51dd5
Gustavo <go609127@gmail.com>
2022-09-05 20:51:44 -0300

    barra de navegação
    iniciei a criação da barra de navegação

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import navbar from './componentes/navbar'
+import NavBar from './componentes/navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <navbar/>
+      <NavBar/>
     </div>
   )
 }
--- app/src/componentes/navbar.jsx
+++ app/src/componentes/navbar.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import 'navbar.css'
+import './navbar.css'
 
-const navbar = () => {
+const NavBar = () => {
     return(
-        <div>
-
+        <div className="navbar">
+            <img className="home mg ig" src="#"/> 
+            <img className="search mg ig" src="#"/> 
+            <img className="movie mg ig" src="#"/> 
+            <img className="series mg ig" src="#"/>
+            <img className="top mg ig" src="#"/> 
+            <img className="games mg ig" src="#"/>
+            <img className="plus mg ig" src="#"/> 
+            <img className="random mg ig" src="#"/>
         </div>
     )
 }
 
-export default navbar
+export default NavBar
