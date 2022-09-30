dc1ec25ad5c1bdbb179aebbdf9b8c07d0ee3ec5c
Gustavo <go609127@gmail.com>
2022-09-30 16:41:20 -0300

    barra de navegação
    terminei a barra de navegação

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
-import './App.css'
-import NavBar from './componentes/navbar'
+import { useState } from 'react';
+import './App.css';
+import NavBar from './componentes/navbar';
+import {Link, Routes, Route} from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
--- app/src/componentes/navbar.jsx
+++ app/src/componentes/navbar.jsx
@@ -1,17 +1,35 @@
 import React from "react";
-import './navbar.css'
+import './navbar.css';
+import search from '../assets/search.png';
+import home from '../assets/home.png';
+import movie from '../assets/film.png';
+import series from '../assets/series.png';
+import plus from '../assets/plus.png';
+import random from '../assets/random.png';
 
 const NavBar = () => {
     return(
         <div className="navbar">
-            <img src="../assets/search.png" className="home mg ig"/> 
-            <img src="../assets/home.png" className="home mg ig"/> 
-            <img src="../assets/movie.png" className="movie mg ig"/> 
-            <img src="../assets/series.png" className="series mg ig"/>
-            <img src="../assets/plus.png" className="plus mg ig"/> 
-            <img src="../assets/random.png" className="random mg ig"/>
+            <div className="items">
+                <img src={search} className="search mg ig"/>
+            </div>
+            <div className="items">
+                <img src={home} className="home mg ig"/> 
+            </div>
+            <div className="items">
+                <img src={movie} className="movie mg ig"/> 
+            </div>
+            <div className="items">
+                <img src={series} className="series mg ig"/>
+            </div>
+            <div className="items">
+                <img src={random} className="random mg ig"/>
+            </div>
+            <div className="items">
+                <img src={plus} className="plus mg ig"/> 
+            </div>
         </div>
     )
-}
+};
 
-export default NavBar
+export default NavBar;
